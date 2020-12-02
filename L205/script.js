0f6103f265e089cc2c9212b5d65df34471619412
khmaladzed <dato.khmaladze2000@gmail.com>
2020-12-02 16:24:55 +0400

    ლაბორატორიული სამუშაო 2

---
+++ L205/script.js
@@ -0,0 +1,14 @@
+for( let j=2; j <=20; j++){
+    //j -> x
+    var isPrime = true;
+
+    for(var i=2;i<j;i++){
+        if(j%i==0){
+            isPrime = false;
+        }
+    }
+
+    if(isPrime){
+        alert(j);
+    }
+}
